be94bc02672ce185207602c0aced2e26bd2f14af
mahbub96 <mahbubcse96@gmail.com>
2022-02-27 01:00:53 +0600

    make it simple and single page application

--- src/pages/Front.js
+++ src/pages/Front.js
@@ -1,14 +1,11 @@
 import React from "react";
-import FrontLeft from "../components/FrontLeft";
 import FrontRight from "../components/FrontRight";
-import classes from "../style/Front.module.css";
 
 function Front() {
   return (
     <>
       <div className="container">
-        <FrontLeft className={`float-left ${classes.left}`} />
-        <FrontRight className={`float-right ${classes.right}`} />
+        <FrontRight className="right" />
       </div>
     </>
   );
