43036180fe8e4c07793b49f522ebe6bafb14fbd6
mahbub96 <mahbubcse96@gmail.com>
2022-02-26 23:30:34 +0600

    developed api response but not debounceing

--- src/components/FrontRight.js
+++ src/components/FrontRight.js
@@ -1,29 +1,65 @@
-import React from "react";
-import DemoWeather from "./DemoWeather";
+import React, { useEffect, useState } from "react";
+import { debounce } from "lodash";
 const logo =
   "https://www.itl.cat/pngfile/big/322-3226139_flowing-water-png-high-resolution-water-splash-png.png";
 
+const API_KEY = "442139248c40c7bfdf976a3b541a09ae";
+
 function FrontRight({ ...rest }) {
+  const [search, setSearch] = useState("Dhaka");
+  const [data, setData] = useState({});
+  const { main } = data;
+
+  if (main !== undefined) {
+    var temp = main.temp;
+    var humidity = main.humidity;
+    var pressure = parseInt(main.pressure) / 1000;
+    var country = data.sys.country;
+    var weatherCondition = data.weather[0].main;
+  } else {
+    pressure = "NaN ";
+    humidity = "NaN ";
+    temp = "NaN ";
+    weatherCondition = "NaN";
+    country = "NaN";
+  }
+
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const link = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${API_KEY}`;
+      const obj = await fetch(link);
+      const jsonObj = await obj.json();
+      setData(jsonObj);
+      // console.log(jsonObj);
+    };
+
+    fetchData();
+  }, [search]);
+
   return (
     <div {...rest}>
       <div className="header">
         <img src={logo} alt="Logo" height="100px" width="100px" />
-        <h3>Dhaka Division,BD</h3>
-        <h5>Haze(haze)</h5>
-        <p>Temp:30 deg Pressure:101325pa Humidity:80%</p>
+        <h3>
+          {search},{country}
+        </h3>
+        <h5>{weatherCondition}</h5>
+        <p>
+          Temp:{temp} deg Pressure:
+          {pressure} atm Humidity:{humidity} %
+        </p>
       </div>
 
       <div className="searchArea">
-        <input type="search" name="search" />
-        <p>After Entering a valid City Name press Enter to get result</p>
-      </div>
+        <input
+          type="search"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          name="search"
+        />
 
-      <div className="searchResult">
-        <h2>Recently Searched Result :</h2>
-        <DemoWeather />
-        <DemoWeather />
-        <DemoWeather />
-        <DemoWeather />
+        <p>After Entering a valid City Name press Enter to get result</p>
       </div>
     </div>
   );
